0c4179e22a79450cad16ed167ed3105fbcc10033
Usuario <yandres24@hotmail.com>
2023-12-06 02:06:26 -0500

    ajustes de arqutectura y diseños xml

---
+++ app/src/main/java/com/yaml/unal_reforaccion_yaml/ui/ForgotPasswordActivity.java
@@ -0,0 +1,16 @@
+package com.yaml.unal_reforaccion_yaml.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.yaml.unal_reforaccion_yaml.R;
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+    }
+}
---
+++ app/src/main/java/com/yaml/unal_reforaccion_yaml/ui/ProfileActivity.java
@@ -0,0 +1,16 @@
+package com.yaml.unal_reforaccion_yaml.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.yaml.unal_reforaccion_yaml.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
